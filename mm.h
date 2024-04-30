e9522de6eac332c7290a6043699ec389ebae2ec1
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-30 00:33:10 -0300

    add some macros

--- mm.h
+++ mm.h
@@ -36,7 +36,11 @@ typedef struct block_meta_data_{
 #define NEXT_META_BLOCK(block_meta_data_ptr) \
     (block_meta_data_ptr->next_block)
 
+#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr) \
+    (block_meta_data_t *)((char *)(block_meta_data_ptr + 1) + block_meta_data_ptr->block_size)
 
+#define PREV_META_BLOCK(block_meta_data_ptr) \
+    (block_meta_data_ptr->prev_block)
 
 #define MAX_FAMILIES_PER_VM_PAGE \
     ((SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)) / sizeof(vm_page_family_t))
