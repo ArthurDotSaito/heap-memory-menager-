33891574bcdb6ce380f511ddd63b8fb4047f89e5
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-30 00:30:09 -0300

    add metablock struct and macros

--- mm.h
+++ mm.h
@@ -2,6 +2,11 @@
 
 #define MM_MAX_STRUCT_NAME 32
 
+typedef enum{
+    MM_FALSE;
+    MM_TRUE
+} vm_bool_t;
+
 typedef struct vm_page_family_
 {
     char struct_name[MM_MAX_STRUCT_NAME];
@@ -13,6 +18,25 @@ typedef struct vm_page_for_families_
     struct vm_page_for_families_ *next;
     vm_page_family_t vm_page_family[0];
 } vm_page_for_families_t;
+
+typedef struct block_meta_data_{
+    vm_bool_t is_free;
+    uint32_t block_size;
+    uint32_t offset;
+    struct block_meta_data_ *prev_block;
+    struct block_meta_data_ *next_block;
+} block_meta_data_t;
+
+#define offset_of(container_structure, field_name) \
+    ((size_t)&(((container_structure *)0)->field_name))
+
+#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr) \
+    ((void *)((char *)block_meta_data_ptr - block_meta_data_ptr->offset))
+
+#define NEXT_META_BLOCK(block_meta_data_ptr) \
+    (block_meta_data_ptr->next_block)
+
+
 
 #define MAX_FAMILIES_PER_VM_PAGE \
     ((SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)) / sizeof(vm_page_family_t))
@@ -26,14 +50,6 @@ typedef struct vm_page_for_families_
              curr++, count++)                                                         \
         {
 #define ITERATE_PAGE_FAMILIES_END(vm_page_for_families_ptr, curr)   }}
-
-#define offset_of(container_structure, field_name) \
-    ((size_t)&(((container_structure *)0)->field_name))
-
-#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr) \
-    ((vm_page_t *)((char *)block_meta_data_ptr - block_meta_data_ptr->offset))
-
-
 
 vm_page_family_t *lookup_page_family_by_name(char *struct_name);
 
